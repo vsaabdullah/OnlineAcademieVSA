d8458b30db4c34ba80ac7a9dba980132e491e86b
abdullah <vsa.abdullah@gmail.com>
2020-03-02 10:46:24 +0100

    bedrijfsadres
    bedrijfsadres geïnstalleerd met succes

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,1 +1,15 @@
 console.log('het bestand Mainscript.js is met succes geladen');
+var bedrijfsnaam = "Online Academie VSA";
+var straatnaam = "Grote Markt";
+var straatnummer= "1";
+var postcode= "3000";
+var gemeente ="Leuven";
+var btwnummer= "BE0999999999";
+var btwplichtig=true;
+var websiteadres="www.itenzo.be";
+var mailadres="vsa.abdullah@gmail.com";
+var bedrijfadres = straatnaam +" "+ straatnummer +" "+ postcode +" "+ gemeente;
+
+document.getElementById("mijnbedrijf").innerHTML = bedrijfsnaam +" "+ bedrijfadres;
+
+   
