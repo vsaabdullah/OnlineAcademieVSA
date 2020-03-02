74cc29fd04b288f9db65920d283e7ff4d796cddd
abdullah <vsa.abdullah@gmail.com>
2020-03-02 11:07:53 +0100

    adres en BTW
    in orde

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,15 +1,19 @@
 console.log('het bestand Mainscript.js is met succes geladen');
-var bedrijfsnaam = "Online Academie VSA";
+var bedrijfsnaam = "Online Academie Abdullah";
 var straatnaam = "Grote Markt";
 var straatnummer= "1";
 var postcode= "3000";
 var gemeente ="Leuven";
-var btwnummer= "BE0999999999";
+var btwcijfers= "0999999999";
 var btwplichtig=true;
 var websiteadres="www.itenzo.be";
 var mailadres="vsa.abdullah@gmail.com";
+var bedrijfslandcode="BE";
+var btwnummer= bedrijfslandcode + btwcijfers;
 var bedrijfadres = straatnaam +" "+ straatnummer +" "+ postcode +" "+ gemeente;
+var linebreak="<br>"
 
-document.getElementById("mijnbedrijf").innerHTML = bedrijfsnaam +" "+ bedrijfadres;
+document.getElementById("mijnbedrijf").innerHTML = bedrijfsnaam + linebreak + bedrijfadres + linebreak+"BTW-nummer:"+btwnummer;;
+
 
    
